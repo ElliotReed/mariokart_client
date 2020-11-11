e940bd786b3bbcadd7ab35846a67888cf7cab6b1
gypsyelliot <elliot@elliotreed.net>
2020-11-11 10:33:03 -0700

    added Notes and cleanup

---
+++ NOTES.md
@@ -0,0 +1,14 @@
+# Notes
+
+## Deployment
+
+Upload
+
+- assets folder
+- index.html
+
+Overwrite if newer.
+
+## TODO
+
+- convert track data to "tracks: {default}..." for move to server data
--- archive/wii/index.js
+++
@@ -1,7 +0,0 @@
-import { wiiCupData } from "../../data/wii/wiiCupData.js";
-import { wiiTrackData } from "../../data/wii/wiiTrackData.js";
-
-export const wiiData = {
-  cups: wiiCupData,
-  tracks: wiiTrackData,
-};
